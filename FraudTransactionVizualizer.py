228e665daf5fac2d81bb631fee42c5806ea1e264
Samueal Dsouza <99087302+Samueal07@users.noreply.github.com>
2024-05-08 16:12:43 +0530

    Update FraudTransactionVizualizer.py

--- FraudTransactionVizualizer.py
+++ FraudTransactionVizualizer.py
@@ -3,7 +3,19 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-import requests  # Use requests to download from Dropbox
+import gdown  # To download from Google Drive
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import requests
 
 # Function to download data from Dropbox
 def download_from_dropbox(url, output_path):
